40dc2e95aa4125e9731f9f1d0077e2e7aec364fb
Chesta Gupta <76441931+chesta123@users.noreply.github.com>
2021-07-10 17:11:09 +0530

    Delete abcd.py

--- abcd.py
+++
@@ -1,8 +0,0 @@
-num = int(input("enter the number for which you want to find the nearest palindrome\n"))
-
-for i in range(num,):
-    i.split()
-    j == i[::-1]
-    if i == j:
-        print(i)
-    i+=1
